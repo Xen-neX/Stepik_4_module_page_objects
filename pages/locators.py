eae8e84d869071b16a87762ba468adc85aae1abc
Xen-neX <elaginnn@gmail.com>
2021-06-18 19:47:35 +0300

    Перенос классов, связанных с логином, в base_page.py. Реализация тестов по логину на странице продукта.

--- pages/locators.py
+++ pages/locators.py
@@ -1,16 +1,28 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators():
+
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
 
-class ProductPageLocators():
+
+class ProductPageLocators:
+    SUCCESS_MESSAGE = (By.CLASS_NAME, "alert-success")
     BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
-    BASKET_ALERT_ADDED = (By.XPATH, "//div[@class='alert alert-safe alert-noicon alert-success  fade in']/div[@class='alertinner ']/strong")
+    BASKET_ALERT_ADDED = (
+        By.XPATH,
+        "//div[@class='alert alert-safe alert-noicon alert-success  fade in']/div[@class='alertinner ']/strong")
     BASKET_ALERT_ADDED_NAME = (By.CSS_SELECTOR, "h1")
-    BASKET_ALERT_PRICE = (By.XPATH, "//div[@class='alert alert-safe alert-noicon alert-info  fade in']/div[@class='alertinner ']/p/strong")
+    BASKET_ALERT_PRICE = (
+        By.XPATH,
+        "//div[@class='alert alert-safe alert-noicon alert-info  fade in']/div[@class='alertinner ']/p/strong")
     BASKET_ALERT_PRICE_NAME = (By.CSS_SELECTOR, "p.price_color")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,8 +1,8 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
-class ProductPage(BasePage):
 
+class ProductPage(BasePage):
 
     def click_basket_button(self):
         # реализуйте проверку, что есть кнопка
@@ -13,7 +13,8 @@ class ProductPage(BasePage):
         # проверка названия продукта
         assert self.browser.find_element(*ProductPageLocators.BASKET_ALERT_ADDED), "Alert about adding not presented"
         assert self.browser.find_element(*ProductPageLocators.BASKET_ALERT_ADDED_NAME), "Product name not presented"
-        assert self.browser.find_element(*ProductPageLocators.BASKET_ALERT_ADDED).text == self.browser.find_element(*ProductPageLocators.BASKET_ALERT_ADDED_NAME).text, "Product names not matched"
+        assert self.browser.find_element(*ProductPageLocators.BASKET_ALERT_ADDED).text == self.browser.find_element(
+            *ProductPageLocators.BASKET_ALERT_ADDED_NAME).text, "Product names not matched"
 
     def should_be_price(self):
         # проверка названия продукта
@@ -21,3 +22,11 @@ class ProductPage(BasePage):
         assert self.browser.find_element(*ProductPageLocators.BASKET_ALERT_PRICE_NAME), "Product price not presented"
         assert self.browser.find_element(*ProductPageLocators.BASKET_ALERT_PRICE).text == self.browser.find_element(
             *ProductPageLocators.BASKET_ALERT_PRICE_NAME).text, "Product prices not matched"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappeared_after_adding_product_to_basket(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
